f970b3f920bb1ed355f3d5163af50cdd69fc0af3
MSI Shamim <64169828+msi-shamim@users.noreply.github.com>
2022-02-19 11:03:42 +0600

    init commit

---
+++ app/src/main/java/com/incrementsinc/ipp_inventory_management/MainActivity.java
@@ -0,0 +1,14 @@
+package com.incrementsinc.ipp_inventory_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
