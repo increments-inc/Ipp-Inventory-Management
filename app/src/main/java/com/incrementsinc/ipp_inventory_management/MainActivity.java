d0db9eec8c17233f4aeb3157d79d67c833bdcf1b
MSI-Shamim <64169828+MSI-Shamim@users.noreply.github.com>
2022-02-19 14:56:01 +0600

    Landing Page 20P
    Landing page completed - 20%
    - Home page : Done
    - Single Product page : Going on

--- app/src/main/java/com/incrementsinc/ipp_inventory_management/MainActivity.java
+++ app/src/main/java/com/incrementsinc/ipp_inventory_management/MainActivity.java
@@ -1,14 +1,44 @@
 package com.incrementsinc.ipp_inventory_management;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.incrementsinc.ipp_inventory_management.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private NavController mNavController;
+    private FragmentContainerView mFragmentContainer;
+    private BottomAppBar mBottomAppBar;
+    private FloatingActionButton mScannerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+        //layout inflation
+        mFragmentContainer = mainBinding.fragmentContainerView;
+        mBottomAppBar = mainBinding.bottomAppBar;
+        mScannerButton = mainBinding.floatingActionButton;
+
+        //init settings
+        mNavController = NavHostFragment.findNavController(mFragmentContainer.getFragment());
+        mScannerButton.setOnClickListener(view -> {
+
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mNavController.navigate(R.id.homeFragment);
     }
 }
